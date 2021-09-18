48212060d002b49f19829c0f148c9fafc9f9f7fd
MaksimGritcenko <zmoaty10@gmail.com>
2021-09-18 22:14:21 +0300

    Updates

--- env.js
+++
@@ -1,9 +0,0 @@
-window.env = {
-  "FIREBASE_API_KEY": "AIzaSyBHsiH_IZP8yDW28cgJaMh1xt-ahwU8m1I",
-  "FIREBASE_AUTH_DOMAIN": "react-dashboard-b2e47.firebaseapp.com",
-  "FIREBASE_PROJECT_ID": "react-dashboard-b2e47",
-  "FIREBASE_STORAGE_BUCKET": "react-dashboard-b2e47.appspot.com",
-  "FIREBASE_MESSAGING_SENDER_ID": "356093860934",
-  "FIREBASE_APP_ID": "1:356093860934:web:1519f3c759ef17fca2fe47",
-  "FIREBASE_MEASUREMENT_ID": "G-8JDX06N4DG"
-};
